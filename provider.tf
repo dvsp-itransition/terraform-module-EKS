b8e4ee05b9657a1e9e4899e1286cc4d5e61d5029
devops <you@example.com>
2024-05-21 16:38:27 +0500

    add

--- provider.tf
+++ provider.tf
@@ -1,37 +1,37 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 5.0"
-#     }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
 
-#     kubernetes = {
-#       source  = "hashicorp/kubernetes"
-#       version = ">= 2.24"
-#     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.24"
+    }
 
-#     helm = {
-#       source  = "hashicorp/helm"
-#       version = "= 2.5.1"
-#     }  
-#   }  
-# }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
+    }  
+  }  
+}
 
-# data "aws_eks_cluster_auth" "eks" {
-#   name = aws_eks_cluster.eks.id
-# }
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.id
+}
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.eks.token
-# }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
 
-# # provider "helm" {
-# #   kubernetes {
-# #     host                   = module.eks.cluster_endpoint
-# #     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-# #     token                  = data.aws_eks_cluster_auth.eks.token
-# #   }
-# # }
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
 
